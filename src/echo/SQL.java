57a383bd80c1dfd7fc3b4e715cd65653b25def48
William Holecek <ramholecek@gmail.com>
2016-02-26 15:28:39 +0100

    Cosmetic Console improvements

--- src/echo/SQL.java
+++ src/echo/SQL.java
@@ -64,6 +64,7 @@ public class SQL {
 			   for(int i=start; i<end; i++)
 					 crud.insert(i, "max" + i, "mustermann" + i,con);
 			   endTime = System.nanoTime();
+			   System.out.println();
 			   long insertTime = endTime - startTime;
 
 			   crud.select(con);
@@ -71,6 +72,7 @@ public class SQL {
 			   startTime = System.nanoTime();
 			    for(int i=start; i<end; i++)
 			    	crud.update("john"+i, "doe"+i, i,con);
+			   System.out.println();
 			   endTime = System.nanoTime();
 			   long updateTime = endTime - startTime;
 
@@ -79,11 +81,13 @@ public class SQL {
 			   startTime = System.nanoTime();
 			   	for(int i=start; i<end; i++)
 			   		crud.delete(i,con);
+			   System.out.println();
 			   endTime = System.nanoTime();
 			   long deleteTime = endTime - startTime;
 
 			   crud.select(con);
 
+			   System.out.println();
 			   System.out.println("insert Time: "+insertTime/sec+" sekunden");
 			   System.out.println("update Time: "+updateTime/sec+" sekunden");
 			   System.out.println("delete Time: "+deleteTime/sec+" sekunden");
